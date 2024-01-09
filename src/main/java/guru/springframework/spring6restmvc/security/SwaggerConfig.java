0d3e6893846d98a1a2f0ef5f71afba3b7377bf61
terry <terry@cleverbits.io>
2024-01-09 23:44:38 +0000

    - optimised imports

--- src/main/java/guru/springframework/spring6restmvc/security/SwaggerConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/security/SwaggerConfig.java
@@ -3,11 +3,8 @@ package guru.springframework.spring6restmvc.security;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springdoc.core.GroupedOpenApi;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
